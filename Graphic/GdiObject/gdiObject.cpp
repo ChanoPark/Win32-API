c0a563041376cae5cec4d3123499c2373e49a0fb
ChanoPark <koc081900@korea.ac.kr>
2022-02-08 15:38:56 +0900

    Update GDIObject

---
+++ Graphic/GdiObject/gdiObject.cpp
@@ -0,0 +1,66 @@
+#include <windows.h>
+#include <TCHAR.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = _T("GdiObject");
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
+	, _In_ LPSTR lpszCmdParam, _In_ int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+	hWndMain = hWnd; // hWnd 정보도 전역변수에 저장!	
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage,
+	WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	HBRUSH MyBrush, OldBrush;
+
+	switch (iMessage) {
+	case WM_CREATE:
+		hWndMain = hWnd;
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		MyBrush = (HBRUSH)GetStockObject(GRAY_BRUSH);
+		OldBrush = (HBRUSH)SelectObject(hdc, MyBrush);
+		Rectangle(hdc, 30, 30, 200, 100);
+		SelectObject(hdc, OldBrush);
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
--- Graphic/README.md
+++ Graphic/README.md
@@ -2,4 +2,8 @@
    #### Client 프로젝트
    * Client : GetClientRect 함수를 통해 작업 영역 얻기    
    👉[블로그에서 자세한 내용 확인하기](https://chanos.tistory.com/entry/Windows-API-Win32-API%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%B4-%EC%9E%91%EC%97%85-%EC%98%81%EC%97%AD-%EC%96%BB%EA%B8%B0)  
+   
+   #### GdiObject 프로젝트
+   * GdiObject : 윈도우에서 기본적으로 제공하는 Stock Object 활용하기
+   👉[블로그에서 자세한 내용 확인하기](https://chanos.tistory.com/entry/Windows-API-Win32-API%EC%9D%98-%EA%B7%B8%EB%9E%98%ED%94%BD-GDI%EC%99%80-%EC%8A%A4%ED%86%A1-%EC%98%A4%EB%B8%8C%EC%A0%9D%ED%8A%B8Stock-Object)  
 <hr/>
